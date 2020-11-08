b47546fe0b7a7538385ce8bf762484332c07561f
vladislv31 <vladislv.k31@gmail.com>
2020-11-08 14:41:25 +0200

    classes to functions

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,47 +1,44 @@
-const { use } = require('../routes')
 const UsersService = require('../services/users.service')
 
-class UserController {
-    async getUsers(req, res) {
-        const users = await UsersService.getUsers()
-        return res.status(200).json(users)
-    }
+async function getUsers(req, res) {
+    const users = await UsersService.getUsers()
+    return res.status(200).json(users)
+}
 
-    async createUser(req, res) {
-        if (req.body.username) {
-            const user = await UsersService.createUser(req.body.username)
-            if (user) {
-                return res.status(200).json(user)
-            } else {
-                return res.status(400).json({message: 'Cant create this user'})    
-            }
-        } else {
-            return res.status(400).json({message: 'Bad request'})
-        }
-    }
-
-    async getUserByID(req, res) {
-        const user = await UsersService.getUserByID(req.params.id)
+async function createUser(req, res) {
+    if (req.body.username) {
+        const user = await UsersService.createUser(req.body.username)
         if (user) {
             return res.status(200).json(user)
         } else {
-            return res.status(400).json({message: 'Cant find user'})
+            return res.status(400).json({message: 'Cant create this user'})    
         }
-    }
-
-    async updateUserByID(req, res) {
-        const user = await UsersService.updateUserByID(req.params.id, req.body.username)
-        if (user) {
-            return res.status(200).json(user)
-        } else {
-            return res.status(400).json({message: 'Cant update user'})
-        }
-    }
-
-    async deleteUserByID(req, res) {
-        await UsersService.deleteUserByID(req.params.id)
-        return res.status(200).json({message: 'User was deleted'})
+    } else {
+        return res.status(400).json({message: 'Bad request'})
     }
 }
 
-module.exports = new UserController()
+async function getUserByID(req, res) {
+    const user = await UsersService.getUserByID(req.params.id)
+    if (user) {
+        return res.status(200).json(user)
+    } else {
+        return res.status(400).json({message: 'Cant find user'})
+    }
+}
+
+async function updateUserByID(req, res) {
+    const user = await UsersService.updateUserByID(req.params.id, req.body.username)
+    if (user) {
+        return res.status(200).json(user)
+    } else {
+        return res.status(400).json({message: 'Cant update user'})
+    }
+}
+
+async function deleteUserByID(req, res) {
+    await UsersService.deleteUserByID(req.params.id)
+    return res.status(200).json({message: 'User was deleted'})
+}
+
+module.exports = {getUsers, createUser, getUserByID, updateUserByID, deleteUserByID}
--- services/users.service.js
+++ services/users.service.js
@@ -1,66 +1,64 @@
 const db = require('../core/db.core')
 
-class UsersService {
-    async getUsers() {
-        const users = await db.query('SELECT * FROM users')
-        return users.rows
-    }
+async function getUsers() {
+    const users = await db.query('SELECT * FROM users')
+    return users.rows
+}
 
-    async createUser(username) {
+async function createUser(username) {
+    const check = await this.checkUsername(username)
+    if (check) {
+        return false
+    } else {
+        const user = await db.query('INSERT INTO users(username) VALUES($1) RETURNING *', [username])
+        return user.rows[0]
+    }
+}
+
+async function getUserByID(id) {
+    try {
+        const users = await db.query('SELECT * FROM users WHERE id = $1', [id])
+        if (users.rows.length > 0) {
+            return users.rows[0]
+        } else {
+            return false
+        }
+    } catch (err) {
+        return false
+    }
+}
+
+async function updateUserByID(id, username) {
+    try {
         const check = await this.checkUsername(username)
         if (check) {
             return false
         } else {
-            const user = await db.query('INSERT INTO users(username) VALUES($1) RETURNING *', [username])
+            const user = await db.query('UPDATE users SET username = $1 WHERE id = $2 RETURNING *', [username, id])
+            console.log(user)
             return user.rows[0]
         }
-    }
-
-    async getUserByID(id) {
-        try {
-            const users = await db.query('SELECT * FROM users WHERE id = $1', [id])
-            if (users.rows.length > 0) {
-                return users.rows[0]
-            } else {
-                return false
-            }
-        } catch (err) {
-            return false
-        }
-    }
-
-    async updateUserByID(id, username) {
-        try {
-            const check = await this.checkUsername(username)
-            if (check) {
-                return false
-            } else {
-                const user = await db.query('UPDATE users SET username = $1 WHERE id = $2 RETURNING *', [username, id])
-                console.log(user)
-                return user.rows[0]
-            }
-        } catch (err) {
-            return false
-        }
-    }
-
-    async deleteUserByID(id) {
-        try {
-            await db.query('DELETE FROM users WHERE id = $1', [id])
-            return true
-        } catch (err) {
-            return false
-        }
-    }
-
-    async checkUsername(username) {
-        const user = await db.query('SELECT * FROM users WHERE username = $1', [username])
-        if (user.rows.length > 0) {
-            return user.rows[0]
-        } else {
-            return false
-        }
+    } catch (err) {
+        return false
     }
 }
 
-module.exports = new UsersService()
+async function deleteUserByID(id) {
+    try {
+        await db.query('DELETE FROM users WHERE id = $1', [id])
+        return true
+    } catch (err) {
+        return false
+    }
+}
+
+async function checkUsername(username) {
+    const user = await db.query('SELECT * FROM users WHERE username = $1', [username])
+    if (user.rows.length > 0) {
+        return user.rows[0]
+    } else {
+        return false
+    }
+}
+
+module.exports = {getUsers, createUser, getUserByID, updateUserByID, deleteUserByID, checkUsername}
